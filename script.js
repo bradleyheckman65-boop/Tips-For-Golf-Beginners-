e2d9a4993205d79aab9e5685b37fa8398ffc72cb
bradleyheckman65-boop <bradleyheckman65@gmail.com>
2026-03-30 12:29:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showTip(){
+alert("Golf Tip: Keep your head down and eyes on the ball during your swing!");
+}
+
+function changeColor(){
+document.body.style.backgroundColor = "lightgreen";
+}
